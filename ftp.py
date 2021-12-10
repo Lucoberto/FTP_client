f8a3c664094f7e562388fd76ea009df1f0f141af
Lucoberto <33008163+Lucoberto@users.noreply.github.com>
2021-12-10 20:59:06 +0100

    Update ftp.py

--- ftp.py
+++ ftp.py
@@ -7,13 +7,20 @@ ftplogin= input("login@login#>> ")
 separandoElLogin=ftplogin.split(",")
 
 #Conexion con el servidor FTP por defecto
-ftpConnect = FTP('0.0.0.0')
+ftpConnect = FTP('furyos.obiterdicta.net')
 print('Connected')
 #incia sesion con usuario y contraseña
-ftpConnect.login(user=separandoEllogin[0],passwd=separandoElLogin[1])
+ftpConnect.login(user=separandoElLogin[0],passwd=separandoElLogin[1])
 print('loged')
 #Lista el directorio del FTP
 ftpConnect.retrlines('LIST')
 
+#Recoge el nombre del archivo
+print('[!] Nombre del archivo o directorio [!]')
+ftpupload= input("login@login#>> ")
 
+#Guardar archivos
+ftpConnect.storbinary("STOR "+ ftpupload , open(ftpupload, 'rb'))
 
+#lista el directorio despues de subir el archivo
+ftpConnect.retrlines('LIST')
